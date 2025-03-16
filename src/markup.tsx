73b692af4b6f3d192f597ad2067a6d7e0ef18b11
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-03-16 08:50:41 -0500

    scuffed it

--- src/markup.tsx
+++ src/markup.tsx
@@ -12,18 +12,19 @@ export interface Post {
 
 export const Markup: FC<{ posts: Post[] }> = ({ posts }: { posts: Post[] }) => {
   const allPosts = posts.map((post) => (
-    <div class="post" key={post.id}>
-      <div class="flex">
-        <img
-          src={post.thumbnailUrl}
-          style={{ width: 24, height: 24 }}
-          alt={post.name}
-        />
-        <h2>{post.name}</h2>
+    <div key={post.id}>
+      <div class="flex pb-2">
+        <img src={post.thumbnailUrl} class="w-8 h-8 mr-4" alt={post.name} />
+        <h2 class="font-bold">{post.name}</h2>
       </div>
       <p>{post.tagline}</p>
     </div>
   ));
 
-  return <div>{allPosts}</div>;
+  return (
+    <div class="flex flex-col gap-4">
+      <h2 class="text-2xl font-bold">🚀 Top 5 oghunt.com launches</h2>
+      {allPosts}
+    </div>
+  );
 };
