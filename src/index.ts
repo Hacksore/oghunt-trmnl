970880f7c98922d6bf9c9341e1c9ab9f3e35af6e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-03-16 07:10:51 -0500

    Basic routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// Installation URL
+// https://api.oghunt.com/oauth/trmnl/new
+//
+// Installation Success Webhook URL
+// https://api.oghunt.com/oauth/trmnl/install
+//
+// Plugin Management URL
+// https://api.oghunt.com/settings
+//
+// Plugin Markup URL
+// https://api.oghunt.com/integrations/trmnl/markup
+//
+// Uninstallation Webhook URL
+// https://api.oghunt.com/hooks/trmnl/uninstall
+
+app.get('/oauth/trmnl/new', (c) => {
+  return c.json({ error: 'not implemented' })
+})
+
+app.get('/oauth/trmnl/install', (c) => {
+  return c.json({ error: 'not implemented' })
+})
+
+app.get('/integrations/trmnl/markup', (c) => {
+  return c.json({ error: 'not implemented' })
+})
+
+app.get('/hooks/trmnl/uninstall', (c) => {
+  return c.json({ error: 'not implemented' })
 })
 
 export default app
